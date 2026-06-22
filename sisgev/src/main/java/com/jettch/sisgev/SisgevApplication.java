c549af93e1539806c0763ab511271930696fab79
niciuoss <viniciussales.dev@gmail.com>
2026-06-22 20:58:38 -0300

    java project

---
+++ sisgev/src/main/java/com/jettch/sisgev/SisgevApplication.java
@@ -0,0 +1,13 @@
+package com.jettch.sisgev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgevApplication.class, args);
+	}
+
+}
---
+++ sisgev/src/test/java/com/jettch/sisgev/SisgevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jettch.sisgev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
